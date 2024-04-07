69b8254ecb541597850a2e942451022fdcfcbedf
jaouad bentahar <jbentaha@jaouad.bentahar@gmail.com>
2024-04-07 17:46:11 +0200

    rename test file

--- src/test/java/org/ben/CalculatorTest.java
+++ src/test/java/org/ben/CalculatorTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ScoringOperationsTest {
+public class CalculatorTest {
 
   private final CalculatorFactory calculatorFactory = new CalculatorFactory(new ScoringOperations());
 
